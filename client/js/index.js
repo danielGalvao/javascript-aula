3e2fa26460de1459e22121e589ca494a0d00dd24
Daniel Galvão <galvaodev@gmail.com>
2016-10-12 01:19:32 -0300

    Fix ux to remove content of inputs

--- client/js/index.js
+++ client/js/index.js
@@ -20,4 +20,9 @@ document.querySelector('.form').addEventListener('submit', function(event){
   tdVolume.textContent = campos[1].value * campos[2].value;
   tr.appendChild(tdVolume);
   tbody.appendChild(tr);
+  campos[0].value = '';
+  campos[1].value = 1;
+  campos[2].value = 0;
+
+  campos[0].focus();
 });
