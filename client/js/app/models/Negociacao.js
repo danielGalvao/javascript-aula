da9259261db044968a285305c216362616c3d0ef
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-10-18 21:56:54 -0200

    Protecting date

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,7 +2,7 @@ class Negociacao {
 
   constructor(data, quantidade, valor){
     this._quantidade = quantidade;
-    this._data = data;
+    this._data = new Date(data.getTime());
     this._valor = valor;
     Object.freeze(this);
   }
@@ -12,7 +12,7 @@ class Negociacao {
   }
 
   get Data(){
-    return this._data;
+    return new Date(this._data.getTime());
   }
 
   get Quantidade(){
