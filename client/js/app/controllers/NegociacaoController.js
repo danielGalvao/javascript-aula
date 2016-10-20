07378b864903cae7dcabd9a56774b57a88b1251a
Daniel Galvão <galvaodev@gmail.com>
2016-10-20 01:13:23 -0200

    Getting values of input

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,5 +2,9 @@ class NegociacaoController {
 
   adiciona(event) {
     event.preventDefault();
+    let $ = document.querySelector.bind(document);
+    let inputQtd = $('#quantidade');
+    let inputData = $('#data');
+    let inputValor = $('#valor');
   }
 }
