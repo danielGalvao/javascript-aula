ceca30be67d35b173e0fe5e82e0566ed1934ebd4
Daniel Galvão <galvaodev@gmail.com>
2016-10-24 00:18:15 -0200

    Creating a new instance of negotiation, and working with date

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,13 +2,18 @@ class NegociacaoController {
 
   constructor() {
     let $ = document.querySelector.bind(document);
-    this.inputQtd = $('#quantidade');
-    this.inputData = $('#data');
-    this.inputValor = $('#valor');
+    this._inputQtd = $('#quantidade');
+    this._inputData = $('#data');
+    this._inputValor = $('#valor');
   }
 
   adiciona(event) {
     event.preventDefault();
-    
+
+    let negociacao = new Negociacao(
+      new Date(this._inputData.value.replace(/-/g, ',')),
+      this._inputQtd.value,
+      this._inputValor.value
+    );
   }
 }
