ad547598ac76321df8fe38dd0215376a489b7475
Daniel Galvão <galvaodev@gmail.com>
2016-10-25 00:34:45 -0200

    Add arrow function removing return of function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,9 +12,8 @@ class NegociacaoController {
     let negociacao = new Negociacao(
       new Date(...this._inputData.value
           .split('-')
-          .map(function(item, indice){
-            return item - indice % 2;
-          })),
+          .map((item, indice) => item - indice % 2)
+      ),
       this._inputQtd.value,
       this._inputValor.value
     );
