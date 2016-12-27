78e3bf9bb3a545221b6043307c2e4e5d3b07f274
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-12-26 22:20:19 -0200

    fix self proxy to negociations

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,19 +6,19 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputValor = $('#valor');
 
-    let self = this;
+    let _this = this;
     this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
       get(target, prop, receiver) {
         if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
           return function() {
             Reflect.apply(target[prop], target, arguments);
-            self._negociacoesView.update(target);
+            _this._negociacoesView.update(target);
           }
         }
         return Reflect.get(target, prop, receiver);
       }
     });
-    
+
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
     this._negociacoesView.update(this._listaNegociacoes);
 
