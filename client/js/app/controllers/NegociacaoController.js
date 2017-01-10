04c55d076f204851119d869da8f85aa46693f09e
Daniel Galvão <galvaodev@gmail.com>
2017-01-09 23:50:01 -0200

    Add controller weeks

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -43,6 +43,26 @@ class NegociacaoController {
       negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
       this._mensagem.texto = 'Negociações importadas com sucesso.';
     });
+
+    service.obterNegociacoesSemanaAnterior((err, negociacoes) => {
+      if(err) {
+        this._mensagem.texto = err;
+        return;
+      }
+
+      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+      this._mensagem.texto = 'Negociações importadas com sucesso.';
+    });
+
+    service.obterNegociacoesSemanaRetrasada((err, negociacoes) => {
+      if(err) {
+        this._mensagem.texto = err;
+        return;
+      }
+
+      negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+      this._mensagem.texto = 'Negociações importadas com sucesso.';
+    });
   }
 
   _criaNegociacao() {
